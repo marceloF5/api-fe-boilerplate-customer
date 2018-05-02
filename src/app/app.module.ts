d763c2ee1afadeea308e3a94262d05ecf288663b
Marcelo <marcelo@pollenapp.com.br>
2018-05-02 19:44:45 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import 'materialize-css';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [    
+    BrowserModule,    
+    MaterializeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
